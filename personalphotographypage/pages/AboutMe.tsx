f42b433f12a2300639bdd0479df00242d3b41811
Adam Polk <adampolk91@gmail.com>
2022-10-27 17:07:13 -0500

    adds styling to match wireframe

--- personalphotographypage/pages/AboutMe.tsx
+++ personalphotographypage/pages/AboutMe.tsx
@@ -1,13 +1,17 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import styles from '../styles/aboutMe.module.css'
 import Header from '../components/Header'
+
 
 const AboutMe: NextPage = () => {
   return (
     <div>
       <Header />
+      <div className={styles.headshot}>
+        <Image src='https://res.cloudinary.com/dijhxevwz/image/upload/v1666906690/timHeadshot_rf9aqm.jpg' width='400px' height='400px' alt=''/>
+      </div>
     </div>
   )
 }
