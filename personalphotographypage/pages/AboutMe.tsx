be7e5ab618ca0423a895e1cc4b32fc84eb3d9cc2
Adam Polk <adampolk91@gmail.com>
2022-11-03 17:14:30 -0500

    adds style fixes for footer and aboutme page

--- personalphotographypage/pages/AboutMe.tsx
+++ personalphotographypage/pages/AboutMe.tsx
@@ -9,7 +9,10 @@ const AboutMe: NextPage = () => {
   return (
     <div>
       <div className={styles.data}>
-        <p>Here be Tim. Painter of Pictures on the canvas we call camera.</p>
+        <p className={styles.text}>
+          Welcome to my page!
+          Among my friends I have always been the person to hold the camera. What started out as a hobby has quickly grown into something more. People want to hold on to their treasured memories, and I have been privilidged enough to help them. Taking photos of these happy moments and beautiful scenes brings me great joy and I am excited to share this passion with you. If you have any questions for me please reach out!
+        </p>
       <div className={styles.headshot}>
         <Image src='https://res.cloudinary.com/dijhxevwz/image/upload/v1666906690/timHeadshot_rf9aqm.jpg' width='400px' height='400px' alt=''/>
       </div>
