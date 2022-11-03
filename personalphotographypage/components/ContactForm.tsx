faa33eae06094a1e4e39b336cbcbf2b8f4bd80af
Jacob Gramer <grameraggie@gmail.com>
2022-11-02 19:22:14 -0500

    Email is set up.  Need to change .env.local variable to the hashed string provided to hide Tim's email address

--- personalphotographypage/components/ContactForm.tsx
+++ personalphotographypage/components/ContactForm.tsx
@@ -7,6 +7,7 @@ export default function ContactForm() {
 
   return (
     <Form id={styles.contact_form} action={`https://formsubmit.co/${process.env.EMAIL}`} method="POST">
+      <input type="hidden" name="_captcha" value="false"/>
       <Form.Group className={styles.client_info}>
         <Form.Label>What type of photos do you need?</Form.Label>
         <Form.Select aria-label="Default select example" name="Genre">
@@ -18,19 +19,19 @@ export default function ContactForm() {
       </Form.Group>
       <Form.Group className={styles.client_info}>
         <Form.Label>First & Last Name</Form.Label>
-        <Form.Control type="input" name="name"/>
+        <Form.Control type="input" name="Name"/>
       </Form.Group>
       <Form.Group className={styles.client_info}>
         <Form.Label>Email</Form.Label>
-        <Form.Control type="text" name="email"/>
+        <Form.Control type="text" name="Email"/>
       </Form.Group>
       <Form.Group className={styles.client_info}>
         <Form.Label>Phone Number</Form.Label>
-        <Form.Control type="text" name="phone"/>
+        <Form.Control type="text" name="Phone"/>
       </Form.Group>
       <Form.Group className={styles.client_info}>
         <Form.Label>Tell us more about what you are looking for</Form.Label>
-        <Form.Control type="textarea" name="more info"/>
+        <Form.Control type="textarea" name="More&nbsp;Info"/>
       </Form.Group>
       <Button className={styles.client_info} id={styles.submit_button} variant="secondary" type="submit">Submit</Button>
     </Form>
