3fcb5068acd7c9e1ffcdf1c88cbf673820ad601d
Adam Polk <adampolk91@gmail.com>
2022-11-08 14:32:25 -0600

    pre cors reinstall

--- personalphotographypage/pages/api/hello.ts
+++ personalphotographypage/pages/api/hello.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import NextCors from 'nextjs-cors';
 
 type Data = {
   name: string
--- personalphotographypage/pages/index.tsx
+++ personalphotographypage/pages/index.tsx
@@ -1,4 +1,6 @@
 import type { NextPage } from 'next';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
@@ -6,6 +8,13 @@ import Header from '../components/Header';
 import ImageGrid from '../components/ImageGrid';
 
 const Home: NextPage = () => {
+  const [gallery, setGallery] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://866426881326464:LFyzvwd3tVNW9MjLdMDxXhxe1Oc@api.cloudinary.com/v1_1/dijhxevwz/resources/image?max_results=100')
+      .then((response) => console.log(response))
+      .catch((error) => console.log(error))
+  })
   return (
     <div className={styles.homeContainer}>
       {/* <h1 className={styles.title}> McMillan Photography </h1> */}
@@ -15,3 +24,17 @@ const Home: NextPage = () => {
 }
 
 export default Home
+
+export async function getServerSideProps(context) {
+  const { name, shared } = context.query;
+
+  const isShared = shared !== undefined;
+
+  // return the properties so they are available in the `Index` component
+  return {
+    props: {
+      name: decodeURI(name),
+      isShared,
+    },
+  };
+}
