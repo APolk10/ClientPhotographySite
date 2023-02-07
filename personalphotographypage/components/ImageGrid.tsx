8c4ea586e500c74b5d1c1e898dc5ec69cc808a7a
Jacob Gramer <grameraggie@gmail.com>
2023-02-07 15:42:30 -0600

    Improves some styling choices in the Portfolio page

--- personalphotographypage/components/ImageGrid.tsx
+++ personalphotographypage/components/ImageGrid.tsx
@@ -30,15 +30,30 @@ const ImageGrid = () => {
     };
   }, []);
 
-  console.log(subset)
-
   return (
-    <div>
-      {subset.map((image: any) =>
-        <div key={image.asset_id}>
-          <img src={image.url} alt="Tim's Pic"  />
+    <div className={styles.portfolios}>
+      <div className={styles.imageContainer}>
+        <div>
+          <h2>Family</h2>
+          These are photos of families that I don't really know
         </div>
-      )}
+        <img src="https://res.cloudinary.com/dbvgiuj6d/image/upload/v1668040388/Tim%20Photography/TMc-31_mvdkm1_yulv13.jpg" alt="Family Pic" />
+      </div>
+      <div className={styles.imageContainer}>
+        <img src="https://res.cloudinary.com/dbvgiuj6d/image/upload/v1668040378/Tim%20Photography/TMc-6_ds7fot_dilcaq.jpg" alt="Wedding Pic" />
+        <div>
+          <h2>Weddings</h2>
+          Behold the ceremony of love
+        </div>
+      </div>
+      <div className={styles.imageContainer}>
+        <div>
+          <h2>Holidays</h2>
+          Just typing stuff down here for the hell of it.
+        </div>
+        <img src="https://res.cloudinary.com/dbvgiuj6d/image/upload/v1668040378/Tim%20Photography/TMc-59_plymrp_x7kesp.jpg" alt="Wedding Pic" />
+      </div>
+
     </div>
   )
 }
