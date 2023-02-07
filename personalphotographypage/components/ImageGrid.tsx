d43af4661819eb635cec72305d3c5abd7a1b8d3d
Jacob Gramer <grameraggie@gmail.com>
2023-02-07 10:17:34 -0600

    Inserts cloudinary images into the places that we had placeholder pictures

--- personalphotographypage/components/ImageGrid.tsx
+++ personalphotographypage/components/ImageGrid.tsx
@@ -1,16 +1,43 @@
 import styles from 'styles/ImageGrid.module.css';
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import axios, { AxiosResponse } from 'axios';
+import { resolve } from 'path';
+import Image from 'next/image';
+import { assert } from 'console';
 
-export default function ImageGrid() {
-  return (
-    <div className={styles.imageContainer}>
-      <div className={styles.grid}>
-        <img src="../starry-night.JPG" alt="Starry Night by Van Gogh"/>
-        <img src="../Stańczyk.jpeg" alt="Starry Night by Van Gogh"/>
-        <img src="../funeral-of-a-viking.jpg" alt="Starry Night by Van Gogh"/>
-        <img src="../sunflowers.jpg" alt="Starry Night by Van Gogh"/>
-        <img src="../water-lilies.jpg" alt="Starry Night by Van Gogh"/>
-        <img src="../monet-bridge.jpg" alt="Starry Night by Van Gogh"/>
-      </div>
-    </div>
-  )
+interface ImageProps {
+  images: any []
 }
+
+const ImageGrid = () => {
+  const [gallery, setGallery]: any [] = useState([]);
+  const [subset, setSubset]: any[] = useState([])
+
+  useEffect(() => {
+    let flag = false;
+    axios.get('http://localhost:3000/api/hello')
+      .then((res: AxiosResponse) => {
+        setGallery(res.data.resources);
+        resolve();
+      })
+      .catch((error) => {
+        console.error(error);
+        flag = true;
+      })
+    if (!flag) {
+      setSubset(gallery.slice(0,10))
+    };
+  }, []);
+
+  return (
+    <div>
+      {subset.map((image: any) =>
+        <Image src={image.url} layout="responsive" width={100} height={100} alt="Image" key={image.asset_id}/>)
+      }
+    </div>
+    )
+
+}
+
+export default ImageGrid;
