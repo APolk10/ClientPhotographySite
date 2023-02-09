aeebdcae1d053eebc6da99c6dc8e7a2d128db084
Jacob Gramer <grameraggie@gmail.com>
2023-02-09 10:58:37 -0600

    Removes all unused import statements

--- personalphotographypage/pages/Portfolio.tsx
+++ personalphotographypage/pages/Portfolio.tsx
@@ -1,9 +1,5 @@
 import type { NextPage } from 'next'
 import ImageGrid from '../components/ImageGrid'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Header from '../components/Header'
 
 const Portfolio: NextPage = () => {
   return (
