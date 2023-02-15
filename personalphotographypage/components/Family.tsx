c97faf3bd205d1857f14bdcea1db497f8fcb3dd8
Jacob Gramer <grameraggie@gmail.com>
2023-02-15 17:28:10 -0600

    Changes file structure so that images are passed to Family.tsx in the form of props rather than being obtained through an api call

--- personalphotographypage/components/Family.tsx
+++ personalphotographypage/components/Family.tsx
@@ -3,34 +3,25 @@ import React, { useState, useEffect } from 'react';
 import axios, { AxiosResponse } from 'axios';
 import { resolve } from 'path';
 import styles from '../styles/images.module.css';
+import Image from 'next/image';
 
-
-const Family = () => {
-  const [gallery, setGallery] = useState([])
-
-  useEffect(() => {
-    axios.post('http://localhost:3000/api/images/pictureAPI', {folder: 'Family'})
-      .then((res: AxiosResponse) => {
-
-        setGallery(res.data.images)
-        resolve();
-      })
-      .catch((error) => {
-        console.error(error);
-      })
-  }, []);
-
-  return (
-    gallery.length > 0 ?
-    <div className={styles.imagesContainer}>
-      {gallery.map((image: any) => {
-        return <img src={image.url} alt="Family photo" key={image.asset_id} className={styles.image}/>
-      })}
-    </div>
-    :
-    <div>No Photos Currently Available</div>
-
-  )
+interface Prop {
+  gallery: any []
 }
 
-export default Family;
+export default function Family({ gallery }: Prop) {
+  let element = gallery.length > 0 ? true : false;
+
+  if (element) {
+    return (
+      <div className={styles.imagesContainer}>
+        {gallery.map((image) => <Image src={image.url} alt="Family photo" key={image.asset_id} width={image.width / 20} height={image.height / 20} className={styles.images}/>)}
+      </div>)
+  } else {
+    return (
+      <div>
+        No photos Currently
+      </div>
+    )
+  }
+}
--- personalphotographypage/pages/FamilyPortfolio.tsx
+++ personalphotographypage/pages/FamilyPortfolio.tsx
@@ -1,11 +1,26 @@
-import type { NextPage } from 'next'
-import Family from '../components/Family'
+import type { InferGetServerSidePropsType, GetServerSideProps, NextPage } from 'next';
+import Family from '../components/Family';
 
+interface Data {
+  images: any []
+}
 
-const FamilyPortfolio: NextPage = () => {
+export default function FamilyPortfolio({ data }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
-    <Family />
+    <Family gallery={data}/>
   )
 }
 
-export default FamilyPortfolio
+export const getServerSideProps: GetServerSideProps<{data: any[]}> = async () => {
+  const response = await fetch('http://localhost:3000/api/images/pictureAPI');
+  const data: Data = await response.json();
+  const images = data.images.filter((image) => {
+    return image.folder === "Tim Photography/Family"
+  })
+
+  return {
+    props: {
+      data: images,
+    },
+  }
+}
