b6cca3791e9b98b01db4e97555e06053efa16640
Adam Polk <adampolk91@gmail.com>
2023-05-04 22:29:18 -0500

    fixes layout of the portfolio page to be uniform for each grid container

--- personalphotographypage/components/ImageGrid.tsx
+++ personalphotographypage/components/ImageGrid.tsx
@@ -10,27 +10,24 @@ const ImageGrid = () => {
       <div className={styles.imageContainer}>
         <div className={styles.gridContainer}>
           <h2>Family</h2>
-          These are photos of families that I don&apos;t really know
         </div>
         <Link href="/FamilyPortfolio">
-          <img src="https://res.cloudinary.com/dbvgiuj6d/image/upload/v1668040388/Tim%20Photography/Family/TMc-31_mvdkm1_yulv13.jpg" alt="Family Pic" />
+          <img className={styles.portfolioImage} src="https://res.cloudinary.com/dbvgiuj6d/image/upload/v1668040388/Tim%20Photography/Family/TMc-31_mvdkm1_yulv13.jpg" alt="Family Pic" />
         </Link>
       </div>
       <div className={styles.imageContainer}>
         <Link href="/WeddingsPortfolio">
-          <img src="https://res.cloudinary.com/dbvgiuj6d/image/upload/v1668040378/Tim%20Photography/Weddings/TMc-6_ds7fot_dilcaq.jpg" alt="Wedding Pic" />
+          <img className={styles.portfolioImage} src="https://res.cloudinary.com/dbvgiuj6d/image/upload/v1668040378/Tim%20Photography/Weddings/TMc-6_ds7fot_dilcaq.jpg" alt="Wedding Pic" />
         </Link>
         <div className={styles.gridContainer}>
           <h2>Weddings</h2>
-          Behold the ceremony of love
         </div>
       </div>
       <div className={styles.imageContainer}>
         <div className={styles.gridContainer}>
-          <h2>Holidays</h2>
-          Just typing stuff down here for the hell of it.
+          <h2>Special Moments</h2>
         </div>
-        <img src="https://res.cloudinary.com/dbvgiuj6d/image/upload/v1668040378/Tim%20Photography/Family/TMc-59_plymrp_x7kesp.jpg" alt="Holiday Pic" />
+        <img className={styles.portfolioImage} src="https://res.cloudinary.com/dbvgiuj6d/image/upload/v1668040378/Tim%20Photography/Family/TMc-59_plymrp_x7kesp.jpg" alt="Holiday Pic" />
       </div>
     </div>
   )
